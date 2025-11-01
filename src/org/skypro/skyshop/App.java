03c34537ed54f9b7eed5dad28a753d15fdb4ec70
387-kl <artur_lychik@mail.ru>
2025-11-01 11:08:54 +0300

    Commit

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,35 @@
+package org.skypro.skyshop;
+
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
+import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.SimpleProduct;
+
+public class App {
+    public static void main(String[] args) {
+        ProductBasket basket = new ProductBasket();
+
+        //Демонстрация классов
+        //Добавление продукта в корзину.
+        basket.addToBasket(new Product("egg", 100));
+        basket.addToBasket(new SimpleProduct("egg", 100));
+
+        //Заполнение корзины
+        basket.addToBasket(new Product("milk", 80));
+        basket.addToBasket(new Product("cookie", 150));
+        basket.addToBasket(new Product("salt", 10));
+        basket.addToBasket(new Product("sugar", 60));
+        basket.addToBasket(new SimpleProduct("milk", 80));
+        basket.addToBasket(new DiscountedProduct("cookie", 150, 10));
+        basket.addToBasket(new FixPriceProduct("vegetable cutter"));
+        basket.addToBasket(new SimpleProduct("sugar", 60));
+
+        //Добавление продукта в заполненную корзину, в которой нет свободного места.
+        basket.addToBasket(new Product("sugar", 60));
+        basket.addToBasket(new SimpleProduct("sugar", 60));
+
+        //Печать содержимого корзины с несколькими товарами.
+        basket.printProductsOfBasket();
+    }
+}
